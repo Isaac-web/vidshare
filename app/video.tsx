1cb7666eab6db90a96c037b81d48af9f674b6816
Isaac-web <kanytakiy@gmail.com>
2025-01-26 13:07:11 +0000

    Add video screen

--- app/video.tsx
+++ app/video.tsx
@@ -1,17 +1,38 @@
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
+import { useVideoPlayer, VideoView } from 'expo-video';
+import { useEvent } from 'expo';
 
-const Video = () => {
+const videoSource =
+  'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
+
+const VideoScreen = () => {
+  const player = useVideoPlayer(videoSource, (player) => {
+    player.loop = true;
+    player.play();
+  });
+
+  // const { isPlaying } = useEvent(player, 'playingChange', {
+  //   isPlaying: player.playing,
+  // });
+
   return (
     <SafeAreaView className="bg-black h-full">
-      <View className="w-full h-full flex justify-center items-center">
-        <Text className="text-white">Play video</Text>
+      <View className="w-full h-full">
+        <VideoView
+          player={player}
+          allowsFullscreen
+          allowsPictureInPicture
+          nativeControls={true}
+          style={{ flex: 1, width: '100%', height: '100%' }}
+        />
       </View>
+
       <StatusBar backgroundColor="#000000" />
     </SafeAreaView>
   );
 };
 
-export default Video;
+export default VideoScreen;
